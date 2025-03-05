1347bfc435b2a734a3e5028d335dff7777184a2f
Min <55426737+choisohan@users.noreply.github.com>
2025-03-05 11:25:43 -0800

    simple lighting shader

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { SkeletonUtils } from 'three/examples/jsm/Addons.js';
-import { RawShaderMaterial, Vector2 , DoubleSide } from 'three';
+import { RawShaderMaterial, Vector2 ,Vector3, Vector4, DoubleSide, SphereGeometry , meshStandardMaterial} from 'three';
 
 export const House =(props) => {
 
@@ -14,8 +14,8 @@ export const House =(props) => {
       props.models.traverse((child) => {
           const i =  parseInt(child.name.match(/-?\d+\.?\d*/g)) 
           if ( child.isMesh && i == props.geo) {
-            const clone =  SkeletonUtils.clone(child); 
-
+            var clone =  SkeletonUtils.clone(child); 
+            
             // Set Position
             clone.position.x = props.position.x;
             clone.position.y = props.position.y; 
@@ -23,9 +23,8 @@ export const House =(props) => {
 
             // Replace Materials
             clone.material= clone.material.map( ( mat, i) =>{
-              return rawMaterial(mat.map, props.UDIM[i] ); 
+              return HouseMaterial(mat.map, props.UDIM[i] ); 
             })
-
 
             setModel( clone )
           }
@@ -37,7 +36,7 @@ export const House =(props) => {
 
 
   if(model){
-    return <primitive object={model} />
+   return <primitive object={model} />
   }
 
 
@@ -45,7 +44,7 @@ export const House =(props) => {
 
 
 
-const rawMaterial =(map,x)=> new RawShaderMaterial({
+export const HouseMaterial = (map, x)=>  new RawShaderMaterial({
   vertexShader: `
   uniform mat4 projectionMatrix;
   uniform mat4 viewMatrix;
@@ -55,6 +54,10 @@ const rawMaterial =(map,x)=> new RawShaderMaterial({
   attribute vec2 uv;
   varying vec2 vUv;
 
+  attribute vec3 normal;
+  varying vec3 vNormal;
+  varying vec3 vPosition;
+
   void main()
   {
       vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -62,27 +65,50 @@ const rawMaterial =(map,x)=> new RawShaderMaterial({
       vec4 projectedPosition = projectionMatrix * viewPosition;
       gl_Position = projectedPosition;
       vUv = uv;
+      vNormal = normalize( normal ); 
+      vPosition = gl_Position.xyz; 
   }
   `,
+
   fragmentShader: `
     precision mediump float;
+    varying vec3 vPosition;
     varying vec2 vUv;
+    varying vec3 vNormal;
 
     uniform sampler2D uTexture; 
     uniform vec2 uUDIM; 
 
-  void main(){
-    // Create a local copy of vUv to modify
-    vec2 modifiedUv = vUv;
-    modifiedUv.x += 0.1* uUDIM.x ; // adjust as needed
+    uniform vec4 LightPosition;
 
-     gl_FragColor = vec4( texture2D( uTexture , modifiedUv).xyz, 1.);
-  }
-  `,
-  uniforms:{
-      uTexture: { value: map },
-      uUDIM : {value: new Vector2(x) }
 
-  },transparent: false, // This disables transparency
-  side: DoubleSide // Optional: Render both sides
+    vec3 phong(vec2 _UV) {
+        vec3 n = normalize(vNormal);
+        vec3 s = normalize(vec3(LightPosition) - vPosition);
+        vec3 v = normalize(vec3(-vPosition));
+        vec3 r = reflect(-s, n);
+
+        float lighting = dot(s, n) ; 
+        lighting = smoothstep( 0.0 , 1.0, lighting ) +.5;
+        lighting= clamp(0., 1., lighting  );
+        vec3 diffuseMap = texture2D( uTexture , _UV).xyz ;
+
+        return diffuseMap * lighting;
+    }
+
+      void main(){
+          // Create a local copy of vUv to modify
+          vec2 modifiedUv = vUv;
+          modifiedUv.x += 0.1* uUDIM.x ; // adjust as needed
+
+          gl_FragColor= vec4( phong(modifiedUv) ,1.);
+      }
+    `,
+    uniforms:{
+        uTexture: { value: map },
+        uUDIM : {value: new Vector2(x) },
+        LightPosition: { value: new Vector4(-500,-500,500, -0) },
+    },
+    //transparent: false, 
+    //side: DoubleSide 
 })
--- src/components/Scenes.jsx
+++ src/components/Scenes.jsx
@@ -1,14 +1,13 @@
-import React,{ useEffect, useState} from 'react'
+import React,{ useEffect, useRef, useState} from 'react'
 import { useLoader , Canvas} from '@react-three/fiber';
 import { FBXLoader } from 'three/examples/jsm/Addons.js';
 import { OrbitControls } from '@react-three/drei';
-import { NearestFilter , ClampToEdgeWrapping} from 'three';
+import {  NearestFilter , ClampToEdgeWrapping, BoxGeometry} from 'three';
 import {House} from './House';
 
 
 export default function Scene(){
     const models = useLoader(FBXLoader, '/models/houses.fbx')
-
 
     useEffect(() => {
 
@@ -18,7 +17,6 @@ export default function Scene(){
                 if (child.isMesh) {
 
                     child.material.forEach(mat => {
-                        mat.map.minFilter = NearestFilter;
                         mat.map.magFilter = NearestFilter;
                         mat.map.generateMipmaps = false;
                         mat.map.wrapS = mat.map.wrapT = ClampToEdgeWrapping; //prevent bleeding
@@ -28,17 +26,24 @@ export default function Scene(){
                 }
               });
         }
+
+
       }, [models]);
 
 
+
+
+
     return (
-        <Canvas style={{width:'100vw',height:'100vh', background: 'white'}}>
+        <Canvas style={{width:'100vw',height:'100vh', background: 'gray'}}  camera={{position: [2,5,7], fov: 50}} >
 
 
             <OrbitControls />
 
             <directionalLight position={[0, 5, 5]} intensity={1} />
             <ambientLight />
+
+
 
             <House models= {models} geo={4} position={{x: 2,y:0, z: 0}} UDIM={[1,0]} />
             <House models= {models} geo={1} position={{x: 0,y:0, z: 0}}  UDIM={[0,1]} />
@@ -47,6 +52,7 @@ export default function Scene(){
             <House models= {models} geo={3} position={{x: 3,y:0, z: 2}} UDIM={[0,1]} />
             <House models= {models} geo={4} position={{x: 0,y:0, z: 2}}  UDIM={[1,0]} />
             <House models= {models} geo={1} position={{x: -3,y:0, z: 2}} UDIM={[0,0]}  />
+
 
         </Canvas>)
 }
