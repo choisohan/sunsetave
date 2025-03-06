e689f53b58410c1a02d37f929abf1185421efd80
Min <55426737+choisohan@users.noreply.github.com>
2025-03-05 18:56:50 -0800

    houseMaterial.js

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { SkeletonUtils } from 'three/examples/jsm/Addons.js';
-import { RawShaderMaterial, Vector2 , Vector4 } from 'three';
+import { Vector2  } from 'three';
 import { useModel } from '../contexts/modelContext';
-
+import { HouseMaterial } from '../shaders/houseMaterial';
 
 export default function House(props){
   const modelContext = useModel();
@@ -50,74 +50,3 @@ export default function House(props){
 
 
 
-export const HouseMaterial = ()=>  new RawShaderMaterial({
-  vertexShader: `
-  uniform mat4 projectionMatrix;
-  uniform mat4 viewMatrix;
-  uniform mat4 modelMatrix;    
-  attribute vec3 position;
-  
-  attribute vec2 uv;
-  varying vec2 vUv;
-
-  attribute vec3 normal;
-  varying vec3 vNormal;
-  varying vec3 vPosition;
-
-  void main()
-  {
-      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-      vec4 viewPosition = viewMatrix * modelPosition;
-      vec4 projectedPosition = projectionMatrix * viewPosition;
-      gl_Position = projectedPosition;
-      vUv = uv;
-      vNormal = normalize( normal ); 
-      vPosition = gl_Position.xyz; 
-  }
-  `,
-
-  fragmentShader: `
-    precision mediump float;
-    varying vec3 vPosition;
-    varying vec2 vUv;
-    varying vec3 vNormal;
-
-    uniform sampler2D uMap; 
-    uniform sampler2D uPaperMap; 
-    uniform vec2 uUDIM; 
-
-    uniform vec4 LightPosition;
-
-
-    vec3 phong(vec2 _UV) {
-        vec3 n = normalize(vNormal);
-        vec3 s = normalize(vec3(LightPosition) - vPosition);
-        vec3 v = normalize(vec3(-vPosition));
-        vec3 r = reflect(-s, n);
-
-        float lighting = dot(s, n) ; 
-        lighting = smoothstep( 0.0 , 1.0, lighting ) +.5;
-        lighting= clamp(0., 1., lighting  );
-        vec3 diffuseMap = texture2D( uMap , _UV).xyz ;
-        float paperMap = texture2D( uPaperMap , fract(vUv  * 10. ) ).x ;
-
-        return diffuseMap * lighting * paperMap ;
-    }
-
-      void main(){
-          // Create a local copy of vUv to modify
-          vec2 modifiedUv = vUv;
-          modifiedUv.x += 0.1* uUDIM.x ; // adjust as needed
-
-          gl_FragColor= vec4( phong(modifiedUv) ,1.);
-      }
-    `,
-    uniforms:{
-        uMap: { value: null },
-        uPaperMap : {value : null } ,
-        uUDIM : {value: new Vector2(0.) },
-        LightPosition: { value: new Vector4(-500,-500,500, -0) },
-    },
-    //transparent: false, 
-    //side: DoubleSide 
-})
---
+++ src/shaders/houseMaterial.js
@@ -0,0 +1,74 @@
+import { RawShaderMaterial, Vector2 , Vector4 } from 'three';
+
+
+export const HouseMaterial = ()=>  new RawShaderMaterial({
+    vertexShader: `
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 modelMatrix;    
+    attribute vec3 position;
+    
+    attribute vec2 uv;
+    varying vec2 vUv;
+  
+    attribute vec3 normal;
+    varying vec3 vNormal;
+    varying vec3 vPosition;
+  
+    void main()
+    {
+        vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+        vec4 viewPosition = viewMatrix * modelPosition;
+        vec4 projectedPosition = projectionMatrix * viewPosition;
+        gl_Position = projectedPosition;
+        vUv = uv;
+        vNormal = normalize( normal ); 
+        vPosition = gl_Position.xyz; 
+    }
+    `,
+  
+    fragmentShader: `
+      precision mediump float;
+      varying vec3 vPosition;
+      varying vec2 vUv;
+      varying vec3 vNormal;
+  
+      uniform sampler2D uMap; 
+      uniform sampler2D uPaperMap; 
+      uniform vec2 uUDIM; 
+  
+      uniform vec4 LightPosition;
+  
+  
+      vec3 phong(vec2 _UV) {
+          vec3 n = normalize(vNormal);
+          vec3 s = normalize(vec3(LightPosition) - vPosition);
+          vec3 v = normalize(vec3(-vPosition));
+          vec3 r = reflect(-s, n);
+  
+          float lighting = dot(s, n) ; 
+          lighting = smoothstep( 0.0 , 1.0, lighting ) +.5;
+          lighting= clamp(0., 1., lighting  );
+          vec3 diffuseMap = texture2D( uMap , _UV).xyz ;
+          float paperMap = texture2D( uPaperMap , fract(vUv  * 10. ) ).x ;
+  
+          return diffuseMap * lighting * paperMap ;
+      }
+  
+        void main(){
+            // Create a local copy of vUv to modify
+            vec2 modifiedUv = vUv;
+            modifiedUv.x += 0.1* uUDIM.x ; // adjust as needed
+  
+            gl_FragColor= vec4( phong(modifiedUv) ,1.);
+        }
+      `,
+      uniforms:{
+          uMap: { value: null },
+          uPaperMap : {value : null } ,
+          uUDIM : {value: new Vector2(0.) },
+          LightPosition: { value: new Vector4(-500,-500,500, -0) },
+      },
+      //transparent: false, 
+      //side: DoubleSide 
+  })
