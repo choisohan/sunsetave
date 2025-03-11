49238b5add53cd5149fe195fa61c36310830579e
Min <55426737+choisohan@users.noreply.github.com>
2025-03-11 15:31:08 -0700

    glitch for ical fetch

--- src/components/ReadCalendar.js
+++ src/components/ReadCalendar.js
@@ -3,27 +3,22 @@ import ICAL from "ical.js";
 
 
 export const fetchCalendar = async (icalUrl) => {
+    const serverURL = 'https://unruly-calm-sorrel.glitch.me'
+    fetch(serverURL+'/fetch-ical',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',  
+        },
+        body:JSON.stringify({
+            url: `https://calendar.google.com/calendar/ical/${icalUrl}%40group.calendar.google.com/public/basic.ics`
+        })
+    })  
+    .then(response => response.json())  
+    .then(data => {
+      console.log('iCal data:', data.icalData);
+    })
+    .catch(error => {
+      console.error('Error fetching iCal data:', error);
+    });
 
-    try {
-        const response = await fetch(icalUrl);
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
-       const text = await response.text();
-       const jcalData = ICAL.parse(text);
-       const comp = new ICAL.Component(jcalData);
-       const vevents = comp.getAllSubcomponents("vevent");
-
-       const parsedEvents = vevents.map(event => {
-           const vevent = new ICAL.Event(event);
-           return {
-               summary: vevent.summary,
-               start: vevent.startDate.toString(),
-               end: vevent.endDate.toString(),
-           };
-       });
-       return text
-
-    } catch (error) {
-        console.error("Error fetching iCalendar:", error);
-    }
 };
