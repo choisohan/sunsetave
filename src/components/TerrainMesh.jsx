2d8f064a92e5b93681cf4a1149b11b3bbc7afa67
Min <55426737+choisohan@users.noreply.github.com>
2025-03-17 14:57:46 -0700

    draw outline on terrain mesh

---
+++ src/components/TerrainMesh.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Vector2 , Color , RawShaderMaterial } from 'three'
+import { useThree } from '@react-three/fiber'
+import { useLoader } from "@react-three/fiber";
+import { FBXLoader } from "three/examples/jsm/Addons.js";
+
+
+export default function TerrainMesh(){
+    const {camera, raycaster} = useThree();
+    const [mesh, setMesh] = useState();
+    const _fbxFile = useLoader(FBXLoader, '/models/terrain.fbx'); 
+    const [selectedObject, setSelctedObject] = useState();
+
+    const meshRef = useRef();
+
+
+    useEffect(()=>{
+        _fbxFile.traverse(child =>{
+            if(child.isMesh){
+                //child.material = new MeshBasicMaterial({ color:'white' });
+
+                const material = new RawShaderMaterial({
+                    vertexShader: `
+                        uniform mat4 projectionMatrix;
+                        uniform mat4 viewMatrix;
+                        uniform mat4 modelMatrix;    
+                        attribute vec3 position;
+                        
+                        attribute vec2 uv;
+                        varying vec2 vUv;
+                    
+                        attribute vec3 normal;
+                        varying vec3 vNormal;
+                        varying vec3 vPosition;
+                    
+                        void main()
+                        {
+                            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+                            vec4 viewPosition = viewMatrix * modelPosition;
+                            vec4 projectedPosition = projectionMatrix * viewPosition;
+                            gl_Position = projectedPosition;
+                            vUv = uv;
+                            vNormal = normalize( normal ); 
+                            vPosition = gl_Position.xyz; 
+                        }
+                  `,
+                  fragmentShader : `
+                    precision mediump float;
+
+                    varying vec2 vUv;
+                    uniform float uThickness;
+                    uniform bool uMouseOver;
+                    uniform vec3 uMouseOverColor; 
+
+                    //https://jsfiddle.net/prisoner849/kmau6591/
+                    float edgeFactor(vec2 p){
+                        vec2 grid = abs(fract(p - 0.5) - 0.5) / uThickness;
+                        return min(grid.x, grid.y);
+                    }
+                    
+                    void main() {
+                            
+                    float a = edgeFactor(vUv);
+                    a = smoothstep(.15,.35, a);
+                    
+                    vec3 color = vec3(.0,.0,.0); 
+                    if(uMouseOver){
+                        color = uMouseOverColor;
+                    }
+                    vec3 lineColor = vec3(1.,1.,1.);
+                    vec3 c = mix(lineColor, color , a);
+                    
+                    gl_FragColor = vec4(c, 1.0);
+                    }
+                                        
+                    
+                    `,
+                    uniforms:{
+                        uThickness :{value: .05},
+                        uMouseOver : {value : false},
+                        uMouseOverColor : {value : new Color(.2,.5,.9)}
+                    }})
+
+                child.material = material; 
+
+
+            }
+        })
+
+        setMesh(_fbxFile);
+
+    },[_fbxFile])
+
+
+
+    const OnMouseOver = (evt)=>{
+        var mouse = new Vector2();
+        var intersects;
+        mouse.x = ( evt.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( evt.clientY / window.innerHeight ) * 2 + 1;
+        raycaster.setFromCamera(mouse, camera);
+        intersects = raycaster.intersectObject(meshRef.current);
+
+        if(!intersects[0])return
+
+        const object = intersects[0].object;
+        
+        if(!object) return; 
+        object.material.uniforms.uMouseOver.value = true; 
+
+        if(selectedObject && selectedObject!=object){
+            selectedObject.material.uniforms.uMouseOver.value = false; 
+        }
+
+
+        setSelctedObject(object);
+    }
+
+    const OnMouseDown = e =>{
+        console.log(  selectedObject.position.x )
+    }
+
+
+
+    if(mesh){
+        return <mesh ref ={meshRef} onPointerMove={OnMouseOver} onPointerDown={OnMouseDown}> 
+            <primitive object={mesh}/>
+        </mesh>
+    }
+
+}
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,10 +1,8 @@
 import { Canvas } from '@react-three/fiber'
-import React, { useEffect, useRef, useState } from 'react'
-import { Vector2 , Vector3, Color , BufferAttribute, MeshBasicMaterial, RawShaderMaterial } from 'three'
-import { useThree } from '@react-three/fiber'
+import React from 'react'
 import { OrbitControls } from '@react-three/drei'
-import { useLoader } from "@react-three/fiber";
-import { FBXLoader } from "three/examples/jsm/Addons.js";
+import TerrainMesh from '../components/TerrainMesh'
+
 
 export default function Test() {
 
@@ -12,7 +10,7 @@ export default function Test() {
     return (
     <div><Canvas style={{width:'100vw', height:'100vh'}}  camera={{position: [15,15,15], fov: 20}} >
         <OrbitControls />
-        <TerrainGeometry />
+        <TerrainMesh />
        
     </Canvas></div>
     )
@@ -21,148 +19,3 @@ export default function Test() {
 
 
 
-
-
-const createVertexColorAttribute = (geometry)=>{
-    const count = geometry.attributes.position.count; 
-    const colors = new Float32Array(count * 3); 
-
-    for (let i = 0; i < count; i++) {
-        colors[i * 3] = 1; 
-        colors[i * 3 + 1] = 1; 
-        colors[i * 3 + 2] = 1; 
-    }
-
-    geometry.setAttribute("color", new BufferAttribute(colors, 3));
-    geometry.attributes.color.needsUpdate = true;
-
-}
-
-
-
-const FaceHoverGeometry = ()=>{
-
-    const {scene,camera, raycaster} = useThree();
-    const meshRef = useRef();
-    const [selectedFaceIndex, setSelectedFaceIndex]= useState(); 
-
-
-    useEffect (()=>{
-        if (!meshRef.current) return;
-        createVertexColorAttribute(meshRef.current.geometry);
-
-    },[])
-
-
-    const OnMouseOver = e=>{
-        var mouse = new Vector2();
-        var intersects;
-        mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-        raycaster.setFromCamera(mouse, camera);
-        intersects = raycaster.intersectObject(meshRef.current);
-
-        if (intersects.length === 0) return;
-
-        const colorAttribute = meshRef.current.geometry.attributes.color;
-    
-        if(colorAttribute){
-
-
-            const previousFaceIndex = selectedFaceIndex; 
-
-            if(previousFaceIndex){
-                //previous
-                var [pv1,pv2,pv3] = GetVertexIndex(meshRef.current.geometry, previousFaceIndex );
-                colorAttribute.setXYZ(pv1, 1, 1, 1);
-                colorAttribute.setXYZ(pv2, 1, 1, 1);
-                colorAttribute.setXYZ(pv3, 1, 1, 1);
-            }
-
-            const faceIndex = intersects[0].faceIndex ; 
-            const [v1,v2,v3] = GetVertexIndex(meshRef.current.geometry, faceIndex );
-            colorAttribute.setXYZ(v1, 0, 0, 0);
-            colorAttribute.setXYZ(v2, 1, 0, 0);
-            colorAttribute.setXYZ(v3, 1, 0, 0);
-
-            setSelectedFaceIndex(faceIndex)
-            colorAttribute.needsUpdate = true; // Update colors
-
-        }
-    }
-
-    const GetVertexIndex = (geometry, faceIndex)=>{
-        const index = geometry.index; // The index buffer (Uint16Array or Uint32Array)
-        const v1 = index.getX(faceIndex * 3);
-        const v2 = index.getX(faceIndex * 3 + 1);
-        const v3 = index.getX(faceIndex * 3 + 2);
-        return [v1, v2, v3]
-    }
-
-
-    return (
-    <mesh ref={meshRef} onPointerMove={OnMouseOver}>
-        <sphereGeometry args={[1, 8, 8]} />
-        <meshBasicMaterial vertexColors   />
-    </mesh>)
-}
-
-
-
-
-
-
-
-const TerrainGeometry = ()=>{
-    const {camera, raycaster} = useThree();
-    const [mesh, setMesh] = useState();
-    const _fbxFile = useLoader(FBXLoader, '/models/terrain.fbx'); 
-    const [selectedObject, setSelctedObject] = useState();
-
-    const meshRef = useRef();
-
-
-    useEffect(()=>{
-        _fbxFile.traverse(child =>{
-            if(child.isMesh){
-              //  createVertexColorAttribute(child.geometry);
-                child.material = new MeshBasicMaterial({ color:'white' });
-
-            }
-        })
-
-        setMesh(_fbxFile);
-
-    },[_fbxFile])
-
-
-
-    const OnMouseOver = (evt)=>{
-        var mouse = new Vector2();
-        var intersects;
-        mouse.x = ( evt.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( evt.clientY / window.innerHeight ) * 2 + 1;
-        raycaster.setFromCamera(mouse, camera);
-        intersects = raycaster.intersectObject(meshRef.current);
-
-        if(!intersects[0])return
-
-        const object = intersects[0].object;
-        
-        if(!object) return; 
-        object.material.color.set('blue');
-
-        if(selectedObject && selectedObject!=object) selectedObject.material.color.set('white');
-
-        setSelctedObject(object);
-    }
-
-
-
-    if(mesh){
-        return <mesh ref ={meshRef} onPointerMove={OnMouseOver}> 
-            <primitive object={mesh}/>
-        </mesh>
-    }
-
-}
