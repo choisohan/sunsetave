622bcbb625ef639a988a181b466252a0c7341ae8
Min <55426737+choisohan@users.noreply.github.com>
2025-04-10 10:13:19 -0700

    Improve event detail

--- src/components/HouseDetailWindow.jsx
+++ src/components/HouseDetailWindow.jsx
@@ -1,52 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Clock} from './Clock'
 import PopupWindow from './PopupWindow'
+import { getCurrentEventIndex } from '../calendar/SortEvents'
 
-
-
+import moment from 'moment-timezone'
 export default function HouseDetailWindow(props) {
   
-  const Header=() => {
-    if(!props.property) return; 
-    return <div className='w-full h-auto'>
-      <span style={{display:'ruby'}} >
-      <a href={`/${props.property.id}`}>
-        <img src='/images/userProfile.png' className='w-[100px]'/>
-      </a>
-      <h3>{props.property.name}</h3>
-      </span>
+  const [property, setProperty] = useState(props.property)
+  const [ currentEventIndex, setCurrentEventIndex] = useState(getCurrentEventIndex(props.property.events));
 
-      <div className='bg-[#748060] p-1 m-1 w-fit border-4 border-black' >
-      <Clock timezone={props.property.timezone}/>
-      {props.property.timezone}
-      </div>
-    </div>
-  }
-
-
-
-  const CurrentEvent=() => {
-    if(!props.property) return; 
-    return <div className='flex-1 bg-gray-200 overflow-y-auto'>Current Event</div>
-  }
-
-
-
-  const NextEvent = ()=>{
-    if(!props.property) return; 
-    return <div className='text-right'> "Next Event" Start in 3 Hours</div>
-    
-  }
-
-
-  if(props.property) {
-    return (
-      <PopupWindow isOpened={true} setIsOpened={()=>{props.onClose()}} >
-          {Header()}
-          {CurrentEvent()}
-          {NextEvent()}
-      </PopupWindow>
-    )
-  }
+  if(!props.property) return null; 
+  return (
+    <PopupWindow isOpened={true} setIsOpened={()=>{props.onClose()}} >
+      <Header id={props.property.id} name={props.property.name} timezone={props.property.timezone}/>
+      <CurrentEvent event={property.events[currentEventIndex]}/>
+      <NextEvent event={property.events[currentEventIndex+1]}/>
+    </PopupWindow>
+  )
+  
 
 }
+
+
+const Header = ({id , name , timezone} )=>{
+    return <div className='w-full h-auto'>
+    <span style={{display:'ruby'}} >
+    <a href={`/${id}`}>
+      <img src='/images/userProfile.png' className='w-[50px]'/>
+    </a>
+    <h3>{name}</h3>
+    </span>
+
+    <div className='bg-[#748060] p-1 m-1 w-fit border-4 border-black' >
+      <Clock timezone={timezone}/>{timezone}
+    </div>
+  </div>
+}
+
+const CurrentEvent=({event}) => {
+  if(!event) return; 
+  return <div className='flex-1 bg-gray-200 overflow-y-auto p-2'>
+    <h3> { event.summary }</h3>
+    <span> { event.description }</span>
+  </div>
+}
+
+const NextEvent = ({event})=>{
+
+  const [hourDiffString, setTimeDiffString] = useState('');
+
+  useEffect(()=>{
+    const diff = event.startMoment.diff( moment() )
+    const duration = moment.duration(Math.abs(diff));
+
+    let unit = 'seconds';
+    let value = Math.floor(duration.asSeconds());
+  
+    if (duration.asMinutes() >= 1) {
+      unit = 'minutes';
+      value = Math.floor(duration.asMinutes());
+    }
+    if (duration.asHours() >= 1) {
+      unit = 'hours';
+      value = Math.floor(duration.asHours());
+    }
+    if (duration.asDays() >= 1) {
+      unit = 'days';
+      value = Math.floor(duration.asDays());
+    }
+  
+    const suffix = diff >= 0 ? 'later' : 'ago';
+
+    if (value === 0) setTimeDiffString('just now');
+    setTimeDiffString(`${value} ${unit} ${suffix}`);
+
+  },[])
+
+  return <div className='text-right'> "{event.summary}" starts {hourDiffString}</div>
+  
+}
+
