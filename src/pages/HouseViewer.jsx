e38ccc4c2e27dfab3b872a89d7fc8ee52bbdfeab
Min <55426737+choisohan@users.noreply.github.com>
2025-04-15 19:59:37 -0700

    css tweak

--- src/pages/HouseViewer.jsx
+++ src/pages/HouseViewer.jsx
@@ -36,7 +36,7 @@ export default function HouseViewer(props) {
 
 
 return (
-<div className="w-full h-screen flex flex-col md:flex-row" >
+<div className="w-full h-screen flex flex-col md:flex-row relative overflow-hidden lg:overflow-auto" >
 
   <Canvas camera={{ position: [0,-5,8], fov: 20}} ref={canvasRef} className="w-full h-full min-h-[500px] md:w-1/2 md:h-[100vh] " >
 
@@ -46,21 +46,21 @@ return (
     <House property={ { id : param  } } onUpdateProperty ={ onUpdateProperty } hoverable={false} onClick={()=>{}} onMouseEnter={()=>{}}/>
   </Canvas>
 
-  <div className="w-full md:w-1/2 p-4">
+  <div className="w-full md:w-1/2 lg:p-4 absolute bottom-0 md:static  ">
 
-    <div>
-      <span className='inline-flex'>
-        <img src='/images/userProfile.png' className='w-[70px] h-[70px]' alt='profile'/>
-        <h2 className='[line-break:anywhere]'>{ name }</h2>
+    <div className='text-right'>
+      <span className='inline-flex '>
+        <img src='/images/userProfile.png' className='w-[35px] h-[35px] lg:w-[70px] lg:h-[70px]' alt='profile'/>
+        <span className='[line-break:anywhere] text-3xl md:text-7xl	'>{ name }</span>
       </span>
 
-      <div className='bg-[#748060] p-1 m-1 w-fit border-4 border-black' >
+      <div className='bg-[#748060] p-1 m-1 w-fit border-4 border-black justify-self-end' >
         <Clock timezone={timezone}/>{timezone} </div>
     </div>
 
 
     
-    <div className='flex column gap-2' style={{marginTop:'10px', marginBottom:'10px'}}>
+    <div className='flex column justify-self-end lg:gap-2 lg:mx-1 lg:my-1'>
       <Buttons.SkipBackwardButton />
       <Buttons.FastForwardButton />
       <Buttons.SkipForwardButton />
